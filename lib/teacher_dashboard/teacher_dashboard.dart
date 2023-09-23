a0ab01e95b524fc64144411b78a469758cf8e170
Ismail Hossain Sepon <21272737+IsmailSepon@users.noreply.github.com>
2023-09-23 03:00:52 +0100

    Teachher Dashboard

--- lib/teacher_dashboard/teacher_dashboard.dart
+++ lib/teacher_dashboard/teacher_dashboard.dart
@@ -1,15 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../component/animated_list_tile.dart';
 
 class TeacherDashBoard  extends StatelessWidget{
   const TeacherDashBoard({super.key});
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   // TODO: implement build
+  //   return Scaffold(
+  //     appBar: AppBar(title: const Text('Dashboard'),),
+  //     body: Container(),
+  //   );
+  // }
+
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: const Text('Teacher Dashboard'),),
-      body: Container(),
+      appBar: AppBar(
+        title: const Text('Add Skill'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('student')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text('No Skill available. Please add your Skill'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (context, index) {
+              var doc = snapshot.data?.docs[index];
+              var data = doc?.data() as Map<String, dynamic>;
+
+              return Padding(
+                padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+                child: AnimatedListTile(
+                  // contentPadding: const EdgeInsets.all(10),
+                  title: Text(data['name']),
+                  trailing: const Icon(Icons.arrow_forward_ios_sharp),
+                  // IconButton(
+                  //   icon: const Icon(Icons.arrow_forward_ios_sharp),
+                  // ),
+                  subtitle: const Text('IF this has any additional information'),
+                  onTap: () {
+                    print('ID: ${doc?.id}');
+                    // context.goNamed("/addSkillDetails", pathParameters: {'id':'${doc?.id}', 'name':'${data['name']}'});
+                    context.go('/details/${data['name']}/${doc?.id}');
+                  },
+
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 
